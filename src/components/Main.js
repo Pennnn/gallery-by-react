bf293ab71ae6fc4d0f1befef734968c8404e8e46
曾坤鹏 <www.524979305@qq.com>
2016-05-25 02:33:08 +0800

    init project

---
+++ src/components/Main.js
@@ -0,0 +1,34 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+let imageDatas = require('../data/imageDatas.json');
+
+function genImageUrl(imageDataArr) {
+    for (let i = 0, j = imageDataArr.length; i < j; i++) {
+        var singleImageData = imageDataArr[i];
+        singleImageData.imageUrl = require('../images/' + singleImageData.filename);
+        imageDataArr[i] = singleImageData;
+    }
+    return imageDataArr;
+}
+imageDatas = genImageUrl(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+        <section className="stage">
+            <section className="img-sec">
+            </section>
+            <nav classname="controller-nav">
+            </nav>
+        </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
